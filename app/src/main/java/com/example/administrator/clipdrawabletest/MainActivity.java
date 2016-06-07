b97ed919bb3143e8356a2e115be19a9c543ed140
ad <ad@itmp.top>
2016-06-07 10:47:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/clipdrawabletest/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.administrator.clipdrawabletest;
+
+import android.graphics.drawable.ClipDrawable;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.os.Message;
+import java.util.Timer;
+import java.util.TimerTask;
+//import java.util.logging.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView)findViewById(R.id.image);
+        final ClipDrawable drawable = (ClipDrawable)imageView.getDrawable();
+        final Handler handler = new Handler()
+        {
+            @Override
+            public void handleMessage(Message msg)
+            {
+                if(msg.what == 0x1233)
+                {
+                    drawable.setLevel(drawable.getLevel()+200);
+                }
+            }
+        };
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Message msg = new Message();
+                msg.what = 0x1233;
+                handler.sendMessage(msg);
+                if(drawable.getLevel()>=10000)
+                {
+                    timer.cancel();
+                }
+            }
+        },0,300);
+    }
+}
